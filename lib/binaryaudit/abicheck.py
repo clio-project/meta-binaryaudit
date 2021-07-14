8505c4f13c72f903de23b9966588773f3bc2ccf7
Anatol Belski <anbelski@linux.microsoft.com>
2021-07-14 21:53:37 +0000

    github: Add QA workflow
    While on it also fix warnings in the abicheck module.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- lib/binaryaudit/abicheck.py
+++ lib/binaryaudit/abicheck.py
@@ -8,12 +8,14 @@ def is_elf(fn):
         head = fd.read(4)
     return head == exp
 
+
 def get_soname_from_xml(xml):
     r = ElementTree.fromstring(xml)
     try:
         return r.attrib["soname"]
     except (AttributeError, KeyError):
         return ""
+
 
 def serialize(fn):
     cmd = ["abidw", "--no-corpus-path", fn]
@@ -22,13 +24,14 @@ def serialize(fn):
     shell = False
     try:
         process = subprocess.Popen(cmd, stdout=sout,
-                       stderr=serr, shell=shell)
+                                   stderr=serr, shell=shell)
         sout, serr = process.communicate()
         out = "".join([out.decode('utf-8') for out in [sout, serr] if out])
-    except OSError as err:
+    except OSError:
         raise
     # return cmd for logging purposes
     return process.returncode, out, cmd
+
 
 def compare(ref, cur):
     cmd = ["abidiff", ref, cur]
@@ -37,13 +40,14 @@ def compare(ref, cur):
     shell = False
     try:
         process = subprocess.Popen(cmd, stdout=sout,
-            stderr=serr, shell=shell)
+                                   stderr=serr, shell=shell)
         sout, serr = process.communicate()
         out = "".join([out.decode('utf-8') for out in [sout, serr] if out])
-    except OSError as err:
+    except OSError:
         raise
     # return cmd for logging purposes
     return process.returncode, out, cmd
+
 
 DIFF_OK = 0
 DIFF_ERROR = 1
@@ -51,20 +55,26 @@ DIFF_USAGE_ERROR = 2
 DIFF_CHANGE = 4
 DIFF_INCOMPATIBLE_CHANGE = 8
 
+
 def diff_is_ok(c):
     return 0 == c
+
 
 def diff_is_error(c):
     return (c & 1) == 1
 
+
 def diff_is_usage_error(c):
     return (c & 2) == 2
+
 
 def diff_is_change(c):
     return (c & 4) == 4
 
+
 def diff_is_incompatible_change(c):
     return (c & 8) == 8
+
 
 def diff_get_bits(c):
     a = []
